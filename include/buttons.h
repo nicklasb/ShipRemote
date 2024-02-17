e859ee57917eadef09c6dca291774e17c60aac12
Nicklas Börjesson <nicklasb@gmail.com>
2024-02-17 18:38:59 +0100

    Implement passably working resistor ladder

--- include/buttons.h
+++ include/buttons.h
@@ -3,7 +3,6 @@
 #include "actions.h"
 
 
-
 e_action_t poll_button_queue();
 
 void init_buttons(char * _log_prefix);
--- src/buttons.c
+++ src/buttons.c
@@ -1,64 +1,111 @@
 
 #include "buttons.h"
 
-#include <freertos/FreeRTOS.h>
-#include <freertos/task.h>
-#include <freertos/queue.h>
-
-#include <button.h>
-
+#include <stdbool.h>
+#include <string.h>
+#include <stdio.h>
 #include <robusto_logging.h>
-#include <communication.h>
+#include <robusto_input.h>
+#include <robusto_concurrency.h>
 #include <robusto_system.h>
 
-static char *button_log_prefix;
-static button_event_t ev;
-static QueueHandle_t button_events;
+resistance_mapping_t resistances[6] = {
+    {.resistance = 176684, .adc_voltage = 2678, .adc_stdev = 4}, //This is the total resistance, or base source_voltage value
+    {.resistance = 92228, .adc_voltage = 2218, .adc_stdev = 5},
+    {.resistance = 42452, .adc_voltage = 2525, .adc_stdev = 3},
+    {.resistance = 18530, .adc_voltage = 2618, .adc_stdev = 4},
+    {.resistance = 7745, .adc_voltage = 2653, .adc_stdev = 2},
+    {.resistance = 3437, .adc_voltage = 2668, .adc_stdev = 3}, 
+};
 
+resistor_monitor_t *monitor = NULL;
+uint32_t change_count = 0;
+
+#define BUTTON_OK 0x01
+#define BUTTON_UP 0x02
+#define BUTTON_LEFT 0x04
+#define BUTTON_DOWN 0x08
+#define BUTTON_RIGHT 0x10
+
+static char *button_log_prefix;
+
+static e_action_t curr_action = ACTION_NONE;
 
 e_action_t poll_button_queue() {
+    e_action_t ret_action = curr_action;
+    curr_action = ACTION_NONE;
+    return ret_action;
+}
 
-    if (xQueueReceive(button_events, &ev, 1000 / portTICK_PERIOD_MS))
+void callback_buttons_press(uint32_t buttons)
+{
+
+    char bit_string[33]; // 32 bits + null terminator
+    for (int i = 0; i < 33; i++)
     {
+        bit_string[i] = (buttons & (1 << i)) ? '0' + i : '_';
+    }
+    bit_string[32] = '\0'; // Null-terminate the string
 
+    ROB_LOGI("LADDER BUTTONS", "Change: %3.lu - Buttons: %s - Value: %lu.", change_count++, bit_string, buttons);
 
-        if ((ev.pin == CONFIG_BUTTON_LEFT_GPIO) && (ev.event == BUTTON_DOWN))
-        {
-            ROB_LOGI(button_log_prefix, "ACTION_AP_LEFT_1");
-            return ACTION_AP_LEFT_1;
-        }
-        else if ((ev.pin == CONFIG_BUTTON_RIGHT_GPIO) && (ev.event == BUTTON_DOWN))
-        {
-            ROB_LOGI(button_log_prefix, "ACTION_AP_RIGHT_1");
-            return ACTION_AP_RIGHT_1;
-        }
-        else if ((ev.pin == CONFIG_BUTTON_LEFT_GPIO) && (ev.event == BUTTON_HELD))
-        {
-            ROB_LOGI(button_log_prefix, "ACTION_AP_LEFT_10");
-            return ACTION_AP_LEFT_10;
-        }
-        else if ((ev.pin == CONFIG_BUTTON_RIGHT_GPIO) && (ev.event == BUTTON_HELD))
-        {
-            ROB_LOGI(button_log_prefix, "ACTION_AP_RIGHT_10");
-            return ACTION_AP_RIGHT_10;
-        }
-        
+    if (buttons == BUTTON_LEFT)
+    {
+        ROB_LOGI(button_log_prefix, "ACTION_AP_LEFT_1");
+        curr_action = ACTION_AP_LEFT_1;
+    }
+    else if (buttons == BUTTON_RIGHT)
+    {
+        ROB_LOGI(button_log_prefix, "ACTION_AP_RIGHT_1");
+        curr_action = ACTION_AP_RIGHT_1;
     } 
-    return ACTION_NONE;
+    else if (buttons == BUTTON_UP)
+    {
+        ROB_LOGI(button_log_prefix, "ACTION_AP_RIGHT_10");
+        curr_action = ACTION_AP_RIGHT_10;
+    } 
+    else if (buttons == BUTTON_DOWN)
+    {
+        ROB_LOGI(button_log_prefix, "ACTION_AP_LEFT_10");
+        curr_action = ACTION_AP_LEFT_10;
+    } 
+};
+
+void init_resistance_mappings()
+{
+    if (monitor)
+    {
+        return;
+    }
+    monitor = robusto_malloc(sizeof(resistor_monitor_t));
+    monitor->mappings = &resistances;
+    monitor->mapping_count = 6;
+    monitor->callback = &callback_buttons_press;
+    monitor->R1 = 41200;
+    monitor->R2_check_resistor = 2200;
+    monitor->source_voltage = 3300; //TODO:  3206;
+    monitor->GPIO = 38; // Usually OK.
+    monitor->name = "Ladder monitor";
+    monitor->ladder_decode = true;
+    monitor->ladder_exclude_count = 0;
+
+    if (robusto_input_add_resistor_monitor(monitor) != ROB_OK)
+    {
+        ROB_LOGE(button_log_prefix, "Failed to initialize buttons!");
+    } else {
+        ROB_LOGI(button_log_prefix, "Buttons initialized!");
+    }
 }
 
 void init_buttons(char *_log_prefix)
 {
     /* TODO: Implementen a robusto debounce variant that:
-    * * posts Robusto UI events to the UI event queue directly.
-    * * is cross platform 
-    * * is in Robusto UI
-    */
+     * * posts Robusto UI events to the UI event queue directly.
+     * * is cross platform
+     * * is in Robusto UI
+     */
     button_log_prefix = _log_prefix;
-
-
     ROB_LOGI(button_log_prefix, "Init button");
-    button_events = pulled_button_init(PIN_BIT(CONFIG_BUTTON_LEFT_GPIO) | PIN_BIT(CONFIG_BUTTON_RIGHT_GPIO), GPIO_PULLUP_ONLY);
+    init_resistance_mappings();
     ROB_LOGI(button_log_prefix, "Buttons initiated.");
-
 }
